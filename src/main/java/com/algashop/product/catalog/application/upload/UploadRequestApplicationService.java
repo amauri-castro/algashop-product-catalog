69409d28159e15907a3695da51b8cd8a7b502cc1
Amauri Castro <amauri.p.castro@gmail.com>
2026-03-30 23:28:24 -0300

    criado interface para abstrair um Storage Provider, testes com implementação Fake

---
+++ src/main/java/com/algashop/product/catalog/application/upload/UploadRequestApplicationService.java
@@ -0,0 +1,47 @@
+package com.algashop.product.catalog.application.upload;
+
+import com.algashop.product.catalog.application.storage.FileReference;
+import com.algashop.product.catalog.application.storage.StorageProvider;
+import com.algashop.product.catalog.application.utility.ImageMediaTypeExtractor;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+
+import java.net.URL;
+import java.time.Duration;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UploadRequestApplicationService {
+
+    private final StorageProvider storageProvider;
+
+    public UploadResponseOutput requestPreSignedUrl(UploadRequestInput input) {
+        MediaType mediaType = ImageMediaTypeExtractor.fromFileName(input.getOriginalFileName());
+        if (!(mediaType.equals(MediaType.IMAGE_JPEG) || mediaType.equals(MediaType.IMAGE_PNG))) {
+            throw new IllegalArgumentException("Invalid image type");
+        }
+
+        String extension = FilenameUtils.getExtension(input.getOriginalFileName());
+        FileReference fileReference = FileReference.builder()
+                .contentLength(input.getContentLength())
+                .contentType(mediaType)
+                .fileName(UUID.randomUUID() + "." + extension)
+                .expiresIn(Duration.ofMinutes(5))
+                .build();
+
+        URL preSignedUrl = storageProvider.requestUploadUrl(fileReference);
+        OffsetDateTime expiresAt = OffsetDateTime.now().plus(fileReference.getExpiresIn());
+
+        return UploadResponseOutput.builder()
+                .uploadSignedUrl(preSignedUrl.toString())
+                .remoteFileName(fileReference.getFileName())
+                .contentLength(fileReference.getContentLength())
+                .contentType(fileReference.getContentType().toString())
+                .expiresAt(expiresAt)
+                .build();
+    }
+}
---
+++ src/main/java/com/algashop/product/catalog/application/upload/UploadResponseOutput.java
@@ -0,0 +1,21 @@
+package com.algashop.product.catalog.application.upload;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class UploadResponseOutput {
+
+    private String uploadSignedUrl;
+    private String remoteFileName;
+    private Long contentLength;
+    private String contentType;
+    private OffsetDateTime expiresAt;
+}
