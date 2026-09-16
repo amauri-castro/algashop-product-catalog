d6b17764f3a4f6f3838d50f8dbb2348e0474270a
Amauri Castro <amauri.p.castro@gmail.com>
2026-09-16 09:02:21 -0300

    alterado security check

--- src/main/java/com/algashop/product/catalog/infrastructure/persistence/SpringDataAuditingConfig.java
+++ src/main/java/com/algashop/product/catalog/infrastructure/persistence/SpringDataAuditingConfig.java
@@ -1,6 +1,6 @@
 package com.algashop.product.catalog.infrastructure.persistence;
 
-import com.algashop.product.catalog.application.security.SecurityCheckApplicationService;
+import com.algashop.product.catalog.application.security.SecurityChecks;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
@@ -25,7 +25,7 @@ public class SpringDataAuditingConfig {
     }
 
     @Bean
-    public AuditorAware<UUID> auditorProvider(SecurityCheckApplicationService securityCheck) {
+    public AuditorAware<UUID> auditorProvider(SecurityChecks securityCheck) {
         return () -> {
             if (!securityCheck.isAuthenticated() || securityCheck.isMachineAuthenticated()) {
                 return Optional.empty();
